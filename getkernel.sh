acc269de30e3766dacb89e22a491aa692af664b2
Jani Monoses <jani@ubuntu.com>
2013-12-05 07:55:26 +0000

    Push to public repo after downloading

--- getkernel.sh
+++ getkernel.sh
@@ -15,7 +15,9 @@ TARGET_KERNEL_UBUNTU_META=linux-image-$DEVICE
 
 PULL_LP_BIN=./pull-lp-bin.py
 
-REPO_PATH=git
+
+# Download and unpack the kernel binary package from Launchpad
+
 
 TARGET_OUT_UBUNTU_KERNEL=tmp-$TARGET_KERNEL_UBUNTU_META
 #rm -Rf $TARGET_OUT_UBUNTU_KERNEL
@@ -31,7 +33,16 @@ $PULL_LP_BIN $kernel_image -o $TARGET_OUT_UBUNTU_KERNEL $TARGET_KERNEL_UBUNTU_SE
 dpkg-deb -x $TARGET_OUT_UBUNTU_KERNEL/linux-image-[0-9]*.deb $TARGET_OUT_UBUNTU_KERNEL
 kernel_version=${kernel_image#linux-image-}
 
-GITDIR=$REPO_PATH/$DEVICE
+
+# Place vmlinuz and the modules under a local git repo/workdir, skipping if no change has been made since last check
+
+LOCAL_REPOS_PATH=git
+PUBLIC_REPOS_PATH=/srv/phablet.ubuntu.com/git/ubuntu/kernels
+REPO_NAME=ubuntu_prebuilt_kernel_$DEVICE
+
+GITDIR=$LOCAL_REPOS_PATH/$REPO_NAME
+PUBGITDIR=$PUBLIC_REPOS_PATH/$REPO_NAME.git
+
 mkdir -p $GITDIR
 
 cp $TARGET_OUT_UBUNTU_KERNEL/boot/vmlinuz-$kernel_version $GITDIR/kernel
@@ -44,3 +55,10 @@ git init
 git add .
 
 git commit -s -m "Add $kernel_image"
+
+#Create and initialize repo if first run
+mkdir -p $PUBGITDIR
+GIT_DIR=$PUBGITDIR git init --bare --shared=group
+
+#Push from local to public repo
+git push $PUBGITDIR master
